68f05dc116dc4d17b730945733f8635f5582be5e
BiveGpe <wiktorrapacz@gmail.com>
2024-12-04 11:26:53 +0100

    Seedowanie bazy

--- src/main/resources/db/seed/V2_2__seed_class.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO class (id, name)
-VALUES
-    (1, '1A'),
-    (2, '1B'),
-    (3, '2A'),
-    (4, '2B');
---
+++ src/main/resources/db/seed/V2_2__seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (id, firstname, lastname, username, password, email, user_role)
+VALUES
+    (1, 'John', 'Doe', 'johndoe', 'password123', 'john.doe@example.com', 'Admin'),
+    (2, 'Alice', 'Smith', 'alicesmith', 'password123', 'alice.smith@example.com', 'Teacher'),
+    (3, 'Bob', 'Johnson', 'bobjohnson', 'password123', 'bob.johnson@example.com', 'Parent'),
+    (4, 'Charlie', 'Brown', 'charlieb', 'password123', 'charlie.brown@example.com', 'Student');
