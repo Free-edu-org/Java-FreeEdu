17cb1fd6773e65d035a4f409b6f2b023e4214bf0
BiveGpe <wiktorrapacz@gmail.com>
2024-12-20 01:23:08 +0100

    DeleteUser Fix

--- src/main/resources/db/migration/V1_10__create_remark_table.sql
+++ src/main/resources/db/migration/V1_10__create_remark_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE remark (
     id int PRIMARY KEY AUTO_INCREMENT,
-    student_id int NOT NULL REFERENCES student(id),
-    teacher_id int NOT NULL REFERENCES teacher(id),
+    student_id int NOT NULL REFERENCES student(id) ON DELETE CASCADE,
+    teacher_id int NOT NULL REFERENCES teacher(id) ON DELETE CASCADE,
     content TEXT NOT NULL,
     add_date DATE NOT NULL
 );
--- src/main/resources/db/migration/V1_8__create_grade_table.sql
+++ src/main/resources/db/migration/V1_8__create_grade_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE grade (
     id int PRIMARY KEY AUTO_INCREMENT,
     value DECIMAL(3, 2) NOT NULL CHECK (value >= 0 AND value <= 6),
-    student_id int NOT NULL REFERENCES student(id),
-    teacher_id int NOT NULL REFERENCES teacher(id),
+    student_id int NOT NULL REFERENCES student(id) ON DELETE CASCADE,
+    teacher_id int NOT NULL REFERENCES teacher(id) ON DELETE CASCADE,
     subject ENUM('Polish', 'Math', 'Geography') NOT NULL,
     add_date DATE NOT NULL
 );
