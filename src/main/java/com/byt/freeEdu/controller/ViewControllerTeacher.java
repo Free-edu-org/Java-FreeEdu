a27de34f7222bc6e0f0b23414d45a3c85fb2b224
s26964-pj <s26964@pjwstk.edu.pl>
2025-01-19 18:05:59 +0100

    Poprawa mainpage

--- src/main/java/com/byt/freeEdu/controller/ViewControllerTeacher.java
+++ src/main/java/com/byt/freeEdu/controller/ViewControllerTeacher.java
@@ -1,10 +1,14 @@
 package com.byt.freeEdu.controller;
 
 import com.byt.freeEdu.controller.userSesion.SessionService;
+import com.byt.freeEdu.mapper.UserMapper;
 import com.byt.freeEdu.model.DTO.ScheduleDto;
+import com.byt.freeEdu.model.DTO.UserDto;
 import com.byt.freeEdu.model.users.Teacher;
+import com.byt.freeEdu.model.users.User;
 import com.byt.freeEdu.service.*;
 import com.byt.freeEdu.service.users.TeacherService;
+import com.byt.freeEdu.service.users.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,26 +22,34 @@ import java.util.List;
 public class ViewControllerTeacher {
 
     private final ScheduleService scheduleService;
+    private final UserService userService;
+    private final UserMapper userMapper;
     private final AttendanceService attendanceService;
     private final GradeService gradeService;
-    private final SchoolClassService schoolClassService;
     private final RemarkService remarkService;
     private final TeacherService teacherService;
     private final SessionService sessionService;
 
-    public ViewControllerTeacher(ScheduleService scheduleService, AttendanceService attendanceService, GradeService gradeService, SchoolClassService schoolClassService, RemarkService remarkService, TeacherService teacherService, SessionService sessionService) {
+    public ViewControllerTeacher(ScheduleService scheduleService, UserService userService, UserMapper userMapper, AttendanceService attendanceService, GradeService gradeService, RemarkService remarkService, TeacherService teacherService, SessionService sessionService) {
         this.scheduleService = scheduleService;
+        this.userService = userService;
+        this.userMapper = userMapper;
         this.attendanceService = attendanceService;
         this.gradeService = gradeService;
-        this.schoolClassService = schoolClassService;
         this.remarkService = remarkService;
         this.teacherService = teacherService;
         this.sessionService = sessionService;
     }
 
     @GetMapping("/mainpage")
-    public Mono<String> mainpageTeacher() {
-        return Mono.just("teacher/teacher_mainpage");
+    public Mono<String> mainpageTeacher(Model model) {
+        return sessionService.getUserId()
+                .flatMap(userId -> {
+                    User user = userService.getUserById(userId);
+                    UserDto userDto = userMapper.toDto(user); // Mapowanie User -> UserDto
+                    model.addAttribute("user", userDto); // Przekazanie DTO do modelu
+                    return Mono.just("teacher/teacher_mainpage");
+                });
     }
 
     @GetMapping("/schedule")
---
+++ src/main/java/com/byt/freeEdu/model/DTO/UserDto.java
@@ -0,0 +1,11 @@
+package com.byt.freeEdu.model.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String role;
+}
