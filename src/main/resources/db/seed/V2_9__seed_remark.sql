fb3a920abe8e1d98abb25258378822fe9b85073c
s26964-pj <s26964@pjwstk.edu.pl>
2025-01-26 01:37:49 +0100

    dodanie rekordów

--- src/main/resources/db/seed/V2_9__seed_remark.sql
+++ src/main/resources/db/seed/V2_9__seed_remark.sql
@@ -1,12 +1,16 @@
 INSERT INTO remark (student_id, teacher_id, content, add_date)
 VALUES
-    (4, 2, 'Great improvement in Math.', '2024-01-16'),
-    (4, 2, 'Needs to focus more on Geography.', '2024-01-17'),
-    (7, 6, 'Excellent performance in Science.', '2024-01-18'),
-    (7, 6, 'Should participate more in class discussions.', '2024-01-19'),
-    (8, 10, 'Outstanding skills in Art.', '2024-01-20'),
-    (8, 10, 'Needs to improve handwriting.', '2024-01-21'),
-    (11, 2, 'Very good at sports.', '2024-01-22'),
-    (11, 2, 'Should work on time management.', '2024-01-23'),
-    (12, 6, 'Shows great interest in History.', '2024-01-24'),
-    (12, 6, 'Needs to be more attentive in class.', '2024-01-25');
+    (4, 2, 'Duży postęp w matematyce.', '2025-01-25'),
+    (4, 2, 'Potrzebuje bardziej skupić się na geografii.', '2025-01-25'),
+    (7, 6, 'Doskonałe wyniki w naukach przyrodniczych.', '2025-01-26'),
+    (7, 6, 'Powinien bardziej angażować się w dyskusje na zajęciach.', '2025-01-26'),
+    (8, 10, 'Wybitne umiejętności w sztuce.', '2025-01-27'),
+    (8, 10, 'Potrzebuje poprawić charakter pisma.', '2025-01-27'),
+    (11, 2, 'Bardzo dobry w sporcie.', '2025-01-25'),
+    (11, 2, 'Powinien popracować nad zarządzaniem czasem.', '2025-01-26'),
+    (12, 6, 'Wykazuje duże zainteresowanie historią.', '2025-01-27'),
+    (12, 6, 'Powinien być bardziej uważny na zajęciach.', '2025-01-27'),
+    (7, 6, 'Bardzo dobrze radzi sobie z eksperymentami w naukach przyrodniczych.', '2025-01-27'),
+    (8, 10, 'Kreatywny projekt artystyczny.', '2025-01-25'),
+    (11, 2, 'Dobry występ w zawodach sportowych.', '2025-01-27'),
+    (12, 6, 'Zadaje ciekawe pytania na lekcjach historii.', '2025-01-26');
