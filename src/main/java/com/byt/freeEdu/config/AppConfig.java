ae552af159f881638c237788299b3b55d68f667a
s26964-pj <s26964@pjwstk.edu.pl>
2025-07-01 23:25:24 +0200

    fix checkstyle pipeline

--- src/main/java/com/byt/freeEdu/config/AppConfig.java
+++ src/main/java/com/byt/freeEdu/config/AppConfig.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "app")
 public class AppConfig {
-    private boolean debugMode;
+  private boolean debugMode;
 
-    public boolean isDebugMode() {
-        return debugMode;
-    }
+  public boolean isDebugMode() {
+    return debugMode;
+  }
 
-    public void setDebugMode(boolean debugMode) {
-        this.debugMode = debugMode;
-    }
+  public void setDebugMode(final boolean debugMode) {
+    this.debugMode = debugMode;
+  }
 }
--- src/main/java/com/byt/freeEdu/controller/userSesion/SessionService.java
+++ src/main/java/com/byt/freeEdu/controller/userSesion/SessionService.java
@@ -1,15 +1,17 @@
 package com.byt.freeEdu.controller.userSesion;
 
-import com.byt.freeEdu.security.UserUtils;
 import org.springframework.stereotype.Service;
+
 import reactor.core.publisher.Mono;
+
+import com.byt.freeEdu.security.UserUtils;
 
 @Service
 public class SessionService {
 
     private final UserUtils userUtils;
 
-    public SessionService(UserUtils userUtils) {
+    public SessionService(final UserUtils userUtils) {
         this.userUtils = userUtils;
     }
 
