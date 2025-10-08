780ceb575315e2aa6b64a866139bf6a807229ed1
s26964-pj <s26964@pjwstk.edu.pl>
2025-10-08 23:24:26 +0200

    remove extra separate

--- src/main/java/com/byt/freeEdu/controller/AdminApiController.java
+++ src/main/java/com/byt/freeEdu/controller/AdminApiController.java
@@ -29,7 +29,6 @@ import com.byt.freeEdu.model.Schedule;
 import com.byt.freeEdu.model.SchoolClass;
 import com.byt.freeEdu.model.enums.SubjectEnum;
 import com.byt.freeEdu.model.users.User;
-
 import com.byt.freeEdu.service.AttendanceService;
 import com.byt.freeEdu.service.GradeService;
 import com.byt.freeEdu.service.RemarkService;
